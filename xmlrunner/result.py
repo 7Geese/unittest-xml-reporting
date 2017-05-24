4e200270cbcafa8c5292529fd09d45b6de72963a
Jonas Trappenberg <jonas@7geese.com>
2017-05-24 10:11:27 -0700

    Fix spacing

--- xmlrunner/result.py
+++ xmlrunner/result.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 import time
@@ -16,28 +15,28 @@ from .unittest import TestResult, _TextTestResult, failfast
 # http://stackoverflow.com/questions/1707890/fast-way-to-filter-illegal-xml-unicode-chars-in-python
 
 _illegal_unichrs = [
-    (0x00, 0x08), (0x0B, 0x0C), (0x0E, 0x1F), 
-    (0x7F, 0x84), (0x86, 0x9F), 
+    (0x00, 0x08), (0x0B, 0x0C), (0x0E, 0x1F),
+    (0x7F, 0x84), (0x86, 0x9F),
     (0xFDD0, 0xFDDF), (0xFFFE, 0xFFFF),
-] 
+]
 if sys.maxunicode >= 0x10000:  # not narrow build 
     _illegal_unichrs.extend([
-        (0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF), 
-        (0x3FFFE, 0x3FFFF), (0x4FFFE, 0x4FFFF), 
-        (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF), 
-        (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF), 
-        (0x9FFFE, 0x9FFFF), (0xAFFFE, 0xAFFFF), 
-        (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF), 
-        (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF), 
+        (0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF),
+        (0x3FFFE, 0x3FFFF), (0x4FFFE, 0x4FFFF),
+        (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF),
+        (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF),
+        (0x9FFFE, 0x9FFFF), (0xAFFFE, 0xAFFFF),
+        (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF),
+        (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF),
         (0xFFFFE, 0xFFFFF), (0x10FFFE, 0x10FFFF),
-    ]) 
+    ])
 
 _illegal_ranges = [
     "%s-%s" % (six.unichr(low), six.unichr(high))
     for (low, high) in _illegal_unichrs
 ]
 
-INVALID_XML_1_0_UNICODE_RE = re.compile(u'[%s]' % u''.join(_illegal_ranges)) 
+INVALID_XML_1_0_UNICODE_RE = re.compile(u'[%s]' % u''.join(_illegal_ranges))
 
 
 STDOUT_LINE = '\nStdout:\n%s'
@@ -351,7 +350,7 @@ class _XMLTestResult(_TextTestResult):
 
         skips = filter(lambda e: e.outcome == _TestInfo.SKIP, tests)
         testsuite.setAttribute('skipped', str(len(list(skips))))
- 
+
         _XMLTestResult._report_testsuite_properties(
             testsuite, xml_document, properties)
 
